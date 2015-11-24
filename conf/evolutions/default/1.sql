0c18206445fda833ee74531034049600f2dd16b7
Patrik Vestergren <patrik.vestergren@digitalroute.com>
2015-11-24 19:33:49 +0100

    conf update

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,4 +1,4 @@
-
+# --- !Ups
 CREATE TABLE LAP (
   driver                    VARCHAR(255) NOT NULL,
   transponder               NUMERIC,
@@ -6,5 +6,42 @@ CREATE TABLE LAP (
   lapTime                   NUMERIC,
   ts                        DATE default now() NOT NULL
 );
-
+# --- !Ups
+CREATE TABLE BESTNLAPS (
+  driver                    VARCHAR(255) NOT NULL,
+  transponder               NUMERIC,
+  totalTime                 NUMERIC,
+  totalTimePres             VARCHAR(25) NOT NULL,
+  tsPres                    VARCHAR(25) NOT NULL,
+  ts                        DATE default now() NOT NULL
+);
+# --- !Ups
+CREATE TABLE BESTMINUTES (
+  driver                    VARCHAR(255) NOT NULL,
+  transponder               NUMERIC,
+  laps                      NUMERIC,
+  totalTime                 NUMERIC,
+  result                    VARCHAR(25) NOT NULL,
+  tsPres                    VARCHAR(25) NOT NULL,
+  ts                        DATE default now() NOT NULL
+);
+# --- !Ups
+CREATE TABLE CURRENTRACER (
+  driver                    VARCHAR(255) NOT NULL,
+  transponder               NUMERIC,
+  lapNr                     NUMERIC,
+  lapTime                   VARCHAR(25) NOT NULL,
+  fastest                   VARCHAR(25) NOT NULL,
+  bestN                     VARCHAR(25) NOT NULL,
+  bestFive                  VARCHAR(25) NOT NULL,
+  tsPres                    VARCHAR(25) NOT NULL,
+  ts                        DATE default now() NOT NULL
+);
+# --- !Downs
 DROP TABLE IF EXISTS LAP;
+# --- !Downs
+DROP TABLE IF EXISTS BESTNLAPS;
+# --- !Downs
+DROP TABLE IF EXISTS BESTMINUTES;
+# --- !Downs
+DROP TABLE IF EXISTS CURRENTRACER;
