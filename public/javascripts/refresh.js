57220d6392e6e964b1a15c5425d6a35f648abcc7
Patrik Vestergren <patrik.vestergren@digitalroute.com>
2016-01-14 22:06:50 +0100

    update counter

--- public/javascripts/refresh.js
+++ public/javascripts/refresh.js
@@ -1,13 +1,13 @@
 
 function refreshDiv() {
     $('.current').load(document.URL +  ' .current');
+    $('.totalLaps').load(document.URL +  ' .totalLaps');
     $('.bestLaps2wd').load(document.URL +  ' .bestLaps2wd');
     $('.bestMinutes2wd').load(document.URL +  ' .bestMinutes2wd');
     $('.bestLaps4wd').load(document.URL +  ' .bestLaps4wd');
     $('.bestMinutes4wd').load(document.URL +  ' .bestMinutes4wd');
     $('.bestLapsUnknown').load(document.URL +  ' .bestLapsUnknown');
     $('.bestMinutesUnknown').load(document.URL +  ' .bestMinutesUnknown');
-    $('.totalLaps').load(document.URL +  ' .totalLaps');
 }
 
 var event = new EventSource("/updates");
